4f9edc456fdf11990d906cc5c8f32e593edbb9bb
ShadowRider7 <sadhinv@gmail.com>
2022-10-20 22:11:06 +0600

    news server

--- index.js
+++ index.js
@@ -3,17 +3,36 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
-app.use(cors());
-
 const categories = require('./data/Categories.json')
+const news = require('./data/news.json')
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('news api running');
 });
 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
 app.get('/categories', (req, res) => {
     res.send(categories);
 });
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === "08") {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
 
 app.listen(port, () => {
     console.log('dragon news server running on port', port);
